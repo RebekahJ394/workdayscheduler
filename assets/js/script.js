e114df78021c0ce3adf3039c5d8e1de3f7ff29f2
Rebekah Jones <rebekahmez@gmail.com>
2021-08-03 23:09:20 -0400

    Script beginning

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var container=$('.container');
+var hourRow=$('#currentDay');
+
+var currentTime= moment().format('MMMM Do YYYY, h:mm:ss a'); 
+
+console.log(currentTime)
+
+var updateToday= setInterval(function() {
+    var today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+    hourRow.html(today);
+}, 1000);
+
+
+
