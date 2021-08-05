efaa59275fe7056a7542d22d9fe8d7ece333db7e
Rebekah Jones <rebekahmez@gmail.com>
2021-08-04 22:02:58 -0400

    div changed in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,3 @@
-var workSchedule = [
-    {
-        id: "0",
-        hour: "09",
-        time: "09",
-        meridiem: " am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: " am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: " am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: " pm",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "01",
-        time: "13",
-        meridiem: " pm",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "02",
-        time: "14",
-        meridiem: " pm",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "03",
-        time: "15",
-        meridiem: " pm",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "04",
-        time: "16",
-        meridiem: " pm",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "05",
-        time: "17",
-        meridiem: " pm",
-        reminder: ""
-    }
-
-]
-
 
 var container=$('.container');
 var hourRow=$('#currentDay');
@@ -78,7 +11,20 @@ var updateToday= setInterval(function() {
     hourRow.html(today);
 }, 1000);
 
+// workSchedule.forEach{function(timeBlock) {
+//     var timeRow = $("<form>").attr( {
+//         "class": "row"
+//     });
+//     $(".container").append(timeRow);
 
+//     var timeBox = ("<div>")
+//         .text('${thisHour.hour}${thisHour.meridiem}')
+//         .attr( {
+//             "class": 
+//         })
+// }
+// }
 var saveButton=$('')
 
 var status = '';
+
